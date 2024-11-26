0690ccccc8cb1fcf291da1b856eebc1b2a64d4ce
lucifer1017 <lucifergrey1017@gmail.com>
2024-11-27 01:50:16 +0530

    route error handling, next fn

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ const app = express();
 const PORT = 8000;
 
 
-app.get("/user", (req, res) => {
-    res.send("got all the users");
+app.get("/user", (req, res, next) => { //this fn is known as route handler
+    console.log("hello");
+    // res.send("got all the users");
+    next();
 })
 app.post("/user", (req, res) => {
     res.send("saved user to db");
+})
+app.get("/user/:userId/:hey/:yoyo", (req, res) => {
+    console.log(req.params);
+    res.send("got all the users");
 })
 app.listen(PORT, () => {
     console.log(`Server running successfully on port: ${PORT}`);
