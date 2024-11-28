fd0dcd22e6dfb6f51b3eac347de0be72fc89a718
lucifer1017 <lucifergrey1017@gmail.com>
2024-11-29 00:14:37 +0530

    express router

--- src/app.js
+++ src/app.js
@@ -1,80 +1,16 @@
 const express = require('express');
 const { connectToDb } = require('./config/database');
-const User = require('./models/user');
-const app = express();
-const bcrypt = require('bcrypt');
-const { validateUserSignup } = require('./utils/validation');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const { userAuth } = require('./middlewares/auth');
-const PORT = 8000;
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.post('/signup', async (req, res) => {
-
-    const data = req?.body;
-    try {
-        validateUserSignup(data);
-        const passwordHash = await bcrypt.hash(data.password, 10);
-        data.password = passwordHash;
-
-        const user = new User(data);
-
-        const result = await user.save();
-        if (!result) {
-            throw new Error("Cannot Save user")
-        }
-        res.send("User added Successfully");
-    } catch (error) {
-        res.status(400).send("Error: " + error.message)
-    }
-
-})
-
-app.post('/login', async (req, res) => {
-    const { email, password } = req?.body;
-    try {
-
-        const user = await User.findOne({ email: email });
-        if (!user) {
-            throw new Error("Invalid Credentials");
-        }
-        const isValidPassword = await user.validatePassword(password);
-        if (!isValidPassword) {
-            throw new Error("Invalid Credentials");
-        }
-        else {
-
-            const token = await user.getJWT();
-
-            res.cookie("token", token, {
-                maxAge: 8 * 72000000
-            });
-
-            res.send("Login Successful");
-        }
-
-    } catch (error) {
-        res.status(400).send("Error: " + error.message)
-    }
-})
-
-app.get('/profile', userAuth, async (req, res) => {
-    try {
-        const user = req.user;
-        res.send(user);
-
-    } catch (error) {
-        res.status(400).send("Error: " + error.message)
-    }
-})
-
-app.post('/sendConnectionRequest', userAuth, async (req, res) => {
-
-    const user = req.user;
-
-    res.send(user?.firstName + " Sent connection request!");
-})
+const PORT = 8000;
+const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile');
+const requestRouter = require('./routes/request');
+app.use('/', authRouter);
+app.use('/', profileRouter);
+app.use('/', requestRouter);
 
 connectToDb()
     .then(() => {
---
+++ src/routes/auth.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const { validateUserSignup } = require('../utils/validation');
+const authRouter = express.Router();
+
+authRouter.post('/signup', async (req, res) => {
+
+    const data = req?.body;
+    try {
+        validateUserSignup(data);
+        const passwordHash = await bcrypt.hash(data.password, 10);
+        data.password = passwordHash;
+
+        const user = new User(data);
+
+        const result = await user.save();
+        if (!result) {
+            throw new Error("Cannot Save user")
+        }
+        res.send("User added Successfully");
+    } catch (error) {
+        res.status(400).send("Error: " + error.message)
+    }
+
+})
+authRouter.post('/login', async (req, res) => {
+    const { email, password } = req?.body;
+    try {
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error("Invalid Credentials");
+        }
+        const isValidPassword = await user.validatePassword(password);
+        if (!isValidPassword) {
+            throw new Error("Invalid Credentials");
+        }
+        else {
+
+            const token = await user.getJWT();
+
+            res.cookie("token", token, {
+                maxAge: 8 * 72000000
+            });
+
+            res.send("Login Successful");
+        }
+
+    } catch (error) {
+        res.status(400).send("Error: " + error.message)
+    }
+})
+
+module.exports = authRouter;
